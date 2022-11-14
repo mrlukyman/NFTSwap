bb4235fb88bde8c385abe7d52cb92f7ca20894d4
Lukas Haring <lukasharing123@gmail.com>
2022-11-14 23:56:16 +0100

    adding back Reac.StrictMode

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -9,10 +9,12 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <BrowserRouter>
-    <GlobalStyle />
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <GlobalStyle />
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
