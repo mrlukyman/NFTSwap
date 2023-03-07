19cd3469af20f2fc726db54be6ffe85d2b20ed8a
Lukas Haring <lukasharing123@gmail.com>
2023-03-07 03:32:58 +0100

    added swap, works only for confirming user A now

--- client/src/components/TradingPanel.tsx
+++ client/src/components/TradingPanel.tsx
@@ -1,8 +1,9 @@
 import { useCallback, useEffect, useState } from 'react'
 import styled from 'styled-components'
 import { useSelector } from 'react-redux'
-import { Alchemy, Network, OwnedNft } from "alchemy-sdk"
+import { Alchemy, Network, OwnedNft, AlchemyProvider, AlchemyConfig } from "alchemy-sdk"
 import Select from 'react-select'
+import { NftSwap } from '@traderxyz/nft-swap-sdk'
 import { NftList } from './NftList'
 import config from '../config.json'
 import { Button, MediumText, Text, Title } from '../styles/GlobalStyles'
@@ -12,6 +13,12 @@ import { ProfileSearch } from './ProfileSearch'
 import { receiverType } from '../types/basicTypes'
 import { useDispatch } from 'react-redux'
 import { removeReceiverInfo } from '../store/receiverSlice'
+import { useSigner } from 'wagmi'
+import signer, { ethers } from 'ethers'
+import ts from 'typescript'
+
+// From your app, provide NftSwap the web3 provider, signer for the user's wallet, and the chain id.
+
 
 const Wrapper = styled.div`
   display: flex;
@@ -149,7 +156,7 @@ const ApproveCheckbox = styled.input`
 
 const settings = {
   apiKey: config.ALCHEMY_API_KEY,
-  network: Network.ETH_MAINNET,
+  network: Network.MATIC_MAINNET,
 }
 
 enum NftListSwitch {
@@ -158,7 +165,6 @@ enum NftListSwitch {
 }
 
 const alchemy = new Alchemy(settings)
-
 
 export const TradingPanel = () => {
   const dispatch = useDispatch()
@@ -175,19 +181,129 @@ export const TradingPanel = () => {
   const [listOfSenderNfts, setListOfSenderNfts] = useState<OwnedNft[]>([])
   const [listOfReceiverNfts, setListReceiverOfNfts] = useState<OwnedNft[]>([])
 
+  const swap = async (userAddress: string) => {
+
+    //@ts-expect-error
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+    const signer = provider.getSigner();
+
+    const CHAIN_ID = 137; //rinkeby
+
+    const waves = {
+      tokenAddress: "0x2953399124f0cbb46d2cbacd8a89cf0599974963",
+      tokenId: "75521973858386990637520923633327380022055191100259040999208965571016910700545",
+      type: "ERC1155",
+    };
+    const line = {
+      tokenAddress: "0x2953399124f0cbb46d2cbacd8a89cf0599974963",
+      tokenId: "92983535497502719937003952852199878577727116762198675210170283774041655345153",
+      type: "ERC1155",
+    };
+    // User A Trade Data
+    const walletAddressUserA = userAddress;
+    const assetsToSwapUserA = [waves];
+
+    // User B Trade Data
+    // const walletAddressUserB = nftHolder;
+    const assetsToSwapUserB = [line];
+    // ............................
+    // Part 1 of the trade -- User A (the 'maker') initiates an order
+    // ............................
+
+    // Initiate the SDK for User A.
+    // Pass the user's wallet signer (available via the user's wallet provider) to the Swap SDK
+    const nftSwapSdk = new NftSwap(provider, signer, CHAIN_ID);
+
+    // Check if we need to approve the NFT for swapping
+    //@ts-expect-error
+    const approvalStatusForUserA = await nftSwapSdk.loadApprovalStatus(assetsToSwapUserA[0], walletAddressUserA);
+
+    // If we do need to approve User A's CryptoPunk for swapping, let's do that now
+    if (!approvalStatusForUserA.contractApproved) {
+      //@ts-expect-error
+      const approvalTx = await nftSwapSdk.approveTokenOrNftByAsset(assetsToSwapUserA[0], walletAddressUserA);
+      const approvalTxReceipt = await approvalTx.wait();
+      console.log(`Approved ${assetsToSwapUserA[0].tokenAddress} contract to swap with 0x (txHash: ${approvalTxReceipt.transactionHash})`);
+    }
+
+    // Create the order (Remember, User A initiates the trade, so User A creates the order)
+    //@ts-expect-error
+    const order = nftSwapSdk.buildOrder(assetsToSwapUserA, assetsToSwapUserB, walletAddressUserA);
+
+    // Sign the order (User A signs since they are initiating the trade)
+    const signedOrder = await nftSwapSdk.signOrder(order, walletAddressUserA);
+    // Part 1 Complete. User A is now done. Now we send the `signedOrder` to User B to complete the trade.
+    return signedOrder;
+    // console.log({
+    //   userAddress,
+    //   userNFT,
+    //   nftHolder,
+    //   nftContract,
+    // });
+  }
+
+  // export async function part2(userAddress, userNFT, makerData) {
+  //   let signedOrder = makerData;
+
+  //   const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+  //   const CHAIN_ID = 4; //rinkeby
+  //   const Aether_420 = {
+  //     tokenAddress: userNFT,
+  //     tokenId: "420",
+  //     type: "ERC721",
+  //   };
+
+  //   // User B Trade Data
+  //   const walletAddressUserB = userAddress;
+  //   const assetsToSwapUserB = [Aether_420];
+  //   // ............................
+  //   // Part 2 of the trade -- User B (the 'taker') accepts and fills order from User A and completes trade
+  //   // ............................
+  //   // Initiate the SDK for User B.
+  //   const signer = provider.getSigner();
+  //   console.log(signer);
+  //   const nftSwapSdk = new NftSwap(provider, signer, CHAIN_ID);
+
+  //   // Check if we need to approve the NFT for swapping
+  //   const approvalStatusForUserB = await nftSwapSdk.loadApprovalStatus(assetsToSwapUserB[0], walletAddressUserB);
+  //   // If we do need to approve NFT for swapping, let's do that now
+  //   if (!approvalStatusForUserB.contractApproved) {
+  //     const approvalTx = await nftSwapSdk.approveTokenOrNftByAsset(assetsToSwapUserB[0], walletAddressUserB);
+  //     const approvalTxReceipt = await approvalTx.wait();
+  //     console.log(`Approved ${assetsToSwapUserB[0].tokenAddress} contract to swap with 0x. TxHash: ${approvalTxReceipt.transactionHash})`);
+  //   }
+  //   // The final step is the taker (User B) submitting the order.
+  //   // The taker approves the trade transaction and it will be submitted on the blockchain for settlement.
+  //   // Once the transaction is confirmed, the trade will be settled and cannot be reversed.
+  //   const fillTx = await nftSwapSdk.fillSignedOrder(signedOrder, undefined, {
+  //     gasLimit: "500000",
+  //     // HACK(johnnrjj) - Rinkeby still has protocol fees, so we give it a little bit of ETH so its happy.
+  //     value: ethers.utils.parseEther("0.01"),
+  //   });
+  //   console.log(fillTx);
+  //   const fillTxReceipt = await nftSwapSdk.awaitTransactionHash(fillTx.hash);
+
+  //   console.log(`🎉 🥳 Order filled. TxHash: ${fillTxReceipt.transactionHash}`);
+
+  //   return fillTxReceipt.transactionHash;
+  // }
+
   const handleMyNftsClicked = useCallback(async () => {
     setSelected(NftListSwitch.MY_NFTS)
     const myNfts = isLoggedin ? await alchemy.nft.getNftsForOwner(senderAddress) : { ownedNfts: [] }
     setListOfNfts(myNfts.ownedNfts)
+    console.log(myNfts.ownedNfts)
     // console.log(myNfts.ownedNfts)
   }, [isLoggedin, senderAddress])
 
   const handleTheirNftsClicked = useCallback(async () => {
     setSelected(NftListSwitch.THEIR_NFTS)
-    const theirNfts = isLoggedin ? await alchemy.nft.getNftsForOwner('effektsvk.eth') : { ownedNfts: [] }
+    const theirNfts = isLoggedin ? await alchemy.nft.getNftsForOwner(receiverWalletAddress) : { ownedNfts: [] }
     console.log(theirNfts.ownedNfts)
     setListOfNfts(theirNfts.ownedNfts)
-  }, [isLoggedin])
+  }, [isLoggedin, receiverWalletAddress])
 
   const handleBack = () => {
     dispatch(removeReceiverInfo())
@@ -247,7 +363,7 @@ export const TradingPanel = () => {
                   <NftList handleNftClicked={handleNftClicked} showShadow={false} nftList={listOfReceiverNfts} interactive size='small' />
                 </MyNftList>
                 <TradeInfoWrapper>
-                  <TradeButton>Trade</TradeButton>
+                  <TradeButton onClick={() => swap(senderAddress)}>Trade</TradeButton>
                 </TradeInfoWrapper>
               </TrdingPanel>
             </TradingPanelWrapper>
