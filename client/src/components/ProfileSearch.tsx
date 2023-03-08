98fde421131d30a1c1c6c5cae3de1a2546504773
Lukas Haring <lukasharing123@gmail.com>
2023-03-08 20:53:40 +0100

    added trading logic to frontend

--- client/src/components/ProfileSearch.tsx
+++ client/src/components/ProfileSearch.tsx
@@ -10,7 +10,11 @@ import styled from 'styled-components'
 import { Colors } from '../styles/Colors'
 import { userSearchType } from '../types/basicTypes'
 import { ProfileSearchCard } from './ProfileSearchCard'
-
+import { useGetUserIncommingOffers } from '../api/hooks/useGetUserIncommingOffers'
+import { NftList } from './NftList'
+import { useGetUser } from '../api/hooks/useGetUser'
+import { part2 } from '../api/swap'
+import { useDeclineOffer } from '../api/hooks/useDeclineOffer'
 
 const GET_USERS = gql`
   query Query {
@@ -19,6 +23,18 @@ const GET_USERS = gql`
       username
     }
   }
+`
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  overflow-y: scroll;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+  -ms-overflow-style: none;
+  scrollbar-width: none;
 `
 
 const AddressWrapper = styled.div`
@@ -37,10 +53,40 @@ const FormWrapper = styled.form`
   justify-content: center;
 `
 
-const ButtonWrapper = styled.button`
-  width: 100%;
-  border: none;
-  background: none;
+const TradeWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 2rem 0 0 0;
+`
+
+const MyNftList = styled.div`
+  border-radius: 1rem;
+  padding: 1rem;
+  width: 45rem; // TODO: make this responsive
+  margin: 0 0 2rem 0;
+  background: ${Colors.cardBackground};
+`
+
+const TradeButton = styled(Button)`
+  background: ${Colors.buttonBackground};
+  width: 10rem;
+  height: 3rem;
+  transition: 0.1s ease-in;
+  color: #fff;
+  font-size: 14px;
+  font-weight: 600;
+  &:hover {
+    background: ${Colors.buttonBackground};
+  }
+`
+
+const TradeButtonWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
 `
 
 export const ProfileSearch = () => {
@@ -50,9 +96,24 @@ export const ProfileSearch = () => {
   const [debouncedReceiver] = useDebounce(receiver, 500)
   const [users, setUsers] = useState<string[]>([])
   const [searchResults, setSearchResults] = useState<string[]>([])
-
+  const [makerUsername, setMakerUsername] = useState<string>('')
+  const [tradeId, setTradeId] = useState<number>()
 
   const [getUsers] = useLazyQuery(GET_USERS)
+  const getUserIncommingOffers = useGetUserIncommingOffers(address)
+  const { getUser } = useGetUser(getUserIncommingOffers?.data.getUserIncommingOffers[0].makerWalletAddress)
+  const { declineOffer } = useDeclineOffer(tradeId)
+
+  const getMakerUsername = useCallback(async () => {
+    getUser()
+      .then((res) => {
+        setMakerUsername(res.data.getUser.username)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }, [getUser])
+
 
   const getAddresses = useCallback(async () => {
     getUsers().then((res) => {
@@ -81,6 +142,22 @@ export const ProfileSearch = () => {
     [users]
   )
 
+  const handleTradeAccept = async (makerData: any) => {
+    const tradeHash = part2(makerData).then((res) => {
+      tradeHash && alert(`🎉 🥳 Order filled. TxHash: ${tradeHash}`)
+      //TODO: add accept offer mutation
+    })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  const handleTradeDecline = (id: any) => {
+    setTradeId(id) //TODO: FIX THIS
+    declineOffer()
+    alert('Offer declined')
+  }
+
   useEffect(() => {
     let result = fuse.search(debouncedReceiver).map(user => user.item)
     setSearchResults(result)
@@ -88,7 +165,8 @@ export const ProfileSearch = () => {
 
   useEffect(() => {
     getAddresses()
-  }, [getAddresses])
+    getMakerUsername()
+  }, [getAddresses, getMakerUsername])
 
   const handleAddressSubmit = (e: any, walletAddress: string, username: string) => {
     e.preventDefault()
@@ -99,30 +177,52 @@ export const ProfileSearch = () => {
     }
   }
   return (
-    <AddressWrapper>
-      <Text>Transfer Address</Text>
-      <SmallText>Enter the username or the address of the user you want to trade with</SmallText>
-      <FormWrapper>
-        <Input
-          type='text'
-          placeholder='@username or 0x...'
-          value={receiver}
-          onChange={(e) => setReceiver(e.target.value)}
-        />
-        {searchResults.map((result: any) => { //TODO: this should be Fuse.FuseResult<userSearchType> (it think) but it doesn't work
-          if (result.walletAddress === address) {
-            return null
-          }
-          return (
-            <ProfileSearchCard
-              key={result.walletAddress}
-              handleAddressSubmit={(e: React.MouseEvent<HTMLElement>) =>
-                handleAddressSubmit(e, result.walletAddress, result.username)}
-              username={result.username} walletAddress={result.walletAddress}
-            />
-          )
-        })}
-      </FormWrapper>
-    </AddressWrapper>
+    <Wrapper>
+      <AddressWrapper>
+        <Text>Transfer Address</Text>
+        <SmallText>Enter the username or the address of the user you want to trade with</SmallText>
+        <FormWrapper>
+          <Input
+            type='text'
+            placeholder='@username or 0x...'
+            value={receiver}
+            onChange={(e) => setReceiver(e.target.value)}
+          />
+          {searchResults.map((result: any) => { //TODO: this should be Fuse.FuseResult<userSearchType> (it think) but it doesn't work
+            if (result.walletAddress === address) {
+              return null
+            }
+            return (
+              <ProfileSearchCard
+                key={result.walletAddress}
+                handleAddressSubmit={(e: React.MouseEvent<HTMLElement>) =>
+                  handleAddressSubmit(e, result.walletAddress, result.username)}
+                username={result.username} walletAddress={result.walletAddress}
+              />
+            )
+          })}
+        </FormWrapper>
+      </AddressWrapper>
+      {getUserIncommingOffers.data.getUserIncommingOffers.filter((nft: any) => nft.status === 'PENDING').map((offer: any) => {
+        return (
+          <TradeWrapper>
+            <MyNftList>
+              <Text>{makerUsername} offered you a trade</Text>
+              <Text>Your NFTs</Text>
+              <NftList nftList={offer.takerNfts} size='small' interactive showShadow={false} />
+            </MyNftList>
+            <MyNftList>
+              <Text>{makerUsername}'s NFTs</Text>
+              <NftList nftList={offer.makerNfts} size='small' interactive showShadow={false} />
+            </MyNftList>
+            <TradeButtonWrapper>
+              <TradeButton onClick={() => handleTradeAccept(offer.makerData)}>Accept</TradeButton>
+              <TradeButton onClick={() => handleTradeDecline(offer.id)}>Decline</TradeButton>
+            </TradeButtonWrapper>
+          </TradeWrapper>
+        )
+      })
+      }
+    </Wrapper>
   )
 }
--- client/src/components/TradingPanel.tsx
+++ client/src/components/TradingPanel.tsx
@@ -3,7 +3,6 @@ import styled from 'styled-components'
 import { useSelector } from 'react-redux'
 import { Alchemy, Network, OwnedNft } from "alchemy-sdk"
 import Select from 'react-select'
-import { NftSwap } from '@traderxyz/nft-swap-sdk'
 import { NftList } from './NftList'
 import config from '../config.json'
 import { Button, MediumText, Text } from '../styles/GlobalStyles'
@@ -13,7 +12,31 @@ import { ProfileSearch } from './ProfileSearch'
 import { receiverType } from '../types/basicTypes'
 import { useDispatch } from 'react-redux'
 import { removeReceiverInfo } from '../store/receiverSlice'
-import { ethers } from 'ethers'
+import { swap, part2 } from '../api/swap'
+import { NftSwap } from '@traderxyz/nft-swap-sdk'
+import { gql, useQuery, useMutation } from '@apollo/client'
+
+const GET_USER_INCOMMING_OFFERS = gql`
+  query Query($walletAddress: String!) {
+    getUserIncommingOffers(walletAddress: $walletAddress) {
+      id
+      makerData
+      makerNfts
+      takerNfts
+      status
+    }
+  }   
+`
+
+const CREATE_OFFER = gql`
+  mutation Mutation($makerWalletAddress: String!, $takerWalletAddress: String!, $makerData: JSON!, $makerNfts: [JSON]!, $takerNfts: [JSON]!) {
+    createOffer(makerWalletAddress: $makerWalletAddress, takerWalletAddress: $takerWalletAddress, makerData: $makerData, makerNfts: $makerNfts, takerNfts: $takerNfts) {
+      makerData
+      makerNfts
+      takerNfts
+    }
+  }
+`
 
 const Wrapper = styled.div`
   display: flex;
@@ -163,11 +186,24 @@ const alchemy = new Alchemy(settings)
 
 export const TradingPanel = () => {
   const dispatch = useDispatch()
-  const senderAddress = useSelector((state: any) => state.user.user.walletAddress)
+  const defaultWalletAddress = useSelector((state: any) => state.user.user.walletAddress)
   const isLoggedin = useSelector((state: any) => state.user.isLoggedin)
   const isSubmitted = useSelector((state: any) => state.trade.isSubmitted)
   const receiverWalletAddress = useSelector((state: any) => state.trade.walletAddress)
   const receiverUsername = useSelector((state: any) => state.trade.username)
+
+  const { data, loading, error } = useQuery(GET_USER_INCOMMING_OFFERS, {
+    variables: { walletAddress: defaultWalletAddress },
+  });
+
+  const [createOffer] = useMutation(CREATE_OFFER, {
+    onCompleted: (data) => {
+      console.log(data)
+    },
+    onError: (error) => {
+      console.log(error)
+    },
+  })
 
   // state for controlling which button is selected
   const [selected, setSelected] = useState(NftListSwitch.MY_NFTS)
@@ -176,122 +212,13 @@ export const TradingPanel = () => {
   const [listOfSenderNfts, setListOfSenderNfts] = useState<OwnedNft[]>([])
   const [listOfReceiverNfts, setListReceiverOfNfts] = useState<OwnedNft[]>([])
 
-  const swap = async (userAddress: string) => {
-
-    //@ts-expect-error
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-
-    const signer = provider.getSigner();
-
-    const CHAIN_ID = 137; //rinkeby
-
-    const waves = {
-      tokenAddress: "0x2953399124f0cbb46d2cbacd8a89cf0599974963",
-      tokenId: "75521973858386990637520923633327380022055191100259040999208965571016910700545",
-      type: "ERC1155",
-    };
-    const line = {
-      tokenAddress: "0x2953399124f0cbb46d2cbacd8a89cf0599974963",
-      tokenId: "92983535497502719937003952852199878577727116762198675210170283774041655345153",
-      type: "ERC1155",
-    };
-    // User A Trade Data
-    const walletAddressUserA = userAddress;
-    const assetsToSwapUserA = [waves];
-
-    // User B Trade Data
-    // const walletAddressUserB = nftHolder;
-    const assetsToSwapUserB = [line];
-    // ............................
-    // Part 1 of the trade -- User A (the 'maker') initiates an order
-    // ............................
-
-    // Initiate the SDK for User A.
-    // Pass the user's wallet signer (available via the user's wallet provider) to the Swap SDK
-    const nftSwapSdk = new NftSwap(provider, signer, CHAIN_ID);
-
-    // Check if we need to approve the NFT for swapping
-    //@ts-expect-error
-    const approvalStatusForUserA = await nftSwapSdk.loadApprovalStatus(assetsToSwapUserA[0], walletAddressUserA);
-
-    // If we do need to approve User A's CryptoPunk for swapping, let's do that now
-    if (!approvalStatusForUserA.contractApproved) {
-      //@ts-expect-error
-      const approvalTx = await nftSwapSdk.approveTokenOrNftByAsset(assetsToSwapUserA[0], walletAddressUserA);
-      const approvalTxReceipt = await approvalTx.wait();
-      console.log(`Approved ${assetsToSwapUserA[0].tokenAddress} contract to swap with 0x (txHash: ${approvalTxReceipt.transactionHash})`);
-    }
-
-    // Create the order (Remember, User A initiates the trade, so User A creates the order)
-    //@ts-expect-error
-    const order = nftSwapSdk.buildOrder(assetsToSwapUserA, assetsToSwapUserB, walletAddressUserA);
-
-    // Sign the order (User A signs since they are initiating the trade)
-    const signedOrder = await nftSwapSdk.signOrder(order, walletAddressUserA);
-    // Part 1 Complete. User A is now done. Now we send the `signedOrder` to User B to complete the trade.
-    return signedOrder;
-    // console.log({
-    //   userAddress,
-    //   userNFT,
-    //   nftHolder,
-    //   nftContract,
-    // });
-  }
-
-  // export async function part2(userAddress, userNFT, makerData) {
-  //   let signedOrder = makerData;
-
-  //   const provider = new ethers.providers.Web3Provider(window.ethereum);
-
-  //   const CHAIN_ID = 4; //rinkeby
-  //   const Aether_420 = {
-  //     tokenAddress: userNFT,
-  //     tokenId: "420",
-  //     type: "ERC721",
-  //   };
-
-  //   // User B Trade Data
-  //   const walletAddressUserB = userAddress;
-  //   const assetsToSwapUserB = [Aether_420];
-  //   // ............................
-  //   // Part 2 of the trade -- User B (the 'taker') accepts and fills order from User A and completes trade
-  //   // ............................
-  //   // Initiate the SDK for User B.
-  //   const signer = provider.getSigner();
-  //   console.log(signer);
-  //   const nftSwapSdk = new NftSwap(provider, signer, CHAIN_ID);
-
-  //   // Check if we need to approve the NFT for swapping
-  //   const approvalStatusForUserB = await nftSwapSdk.loadApprovalStatus(assetsToSwapUserB[0], walletAddressUserB);
-  //   // If we do need to approve NFT for swapping, let's do that now
-  //   if (!approvalStatusForUserB.contractApproved) {
-  //     const approvalTx = await nftSwapSdk.approveTokenOrNftByAsset(assetsToSwapUserB[0], walletAddressUserB);
-  //     const approvalTxReceipt = await approvalTx.wait();
-  //     console.log(`Approved ${assetsToSwapUserB[0].tokenAddress} contract to swap with 0x. TxHash: ${approvalTxReceipt.transactionHash})`);
-  //   }
-  //   // The final step is the taker (User B) submitting the order.
-  //   // The taker approves the trade transaction and it will be submitted on the blockchain for settlement.
-  //   // Once the transaction is confirmed, the trade will be settled and cannot be reversed.
-  //   const fillTx = await nftSwapSdk.fillSignedOrder(signedOrder, undefined, {
-  //     gasLimit: "500000",
-  //     // HACK(johnnrjj) - Rinkeby still has protocol fees, so we give it a little bit of ETH so its happy.
-  //     value: ethers.utils.parseEther("0.01"),
-  //   });
-  //   console.log(fillTx);
-  //   const fillTxReceipt = await nftSwapSdk.awaitTransactionHash(fillTx.hash);
-
-  //   console.log(`🎉 🥳 Order filled. TxHash: ${fillTxReceipt.transactionHash}`);
-
-  //   return fillTxReceipt.transactionHash;
-  // }
-
   const handleMyNftsClicked = useCallback(async () => {
     setSelected(NftListSwitch.MY_NFTS)
-    const myNfts = isLoggedin ? await alchemy.nft.getNftsForOwner(senderAddress) : { ownedNfts: [] }
+    const myNfts = isLoggedin ? await alchemy.nft.getNftsForOwner(defaultWalletAddress) : { ownedNfts: [] }
     setListOfNfts(myNfts.ownedNfts)
     console.log(myNfts.ownedNfts)
     // console.log(myNfts.ownedNfts)
-  }, [isLoggedin, senderAddress])
+  }, [isLoggedin, defaultWalletAddress])
 
   const handleTheirNftsClicked = useCallback(async () => {
     setSelected(NftListSwitch.THEIR_NFTS)
@@ -310,15 +237,39 @@ export const TradingPanel = () => {
   const handleNftClicked = (nft: OwnedNft) => {
     if (selected === NftListSwitch.MY_NFTS) {
       if (listOfSenderNfts.some((nftInList) => nftInList.tokenId === nft.tokenId)) {
-        return
+        //remove nft from list
+        setListOfSenderNfts(listOfSenderNfts.filter((nftInList) => nftInList.tokenId !== nft.tokenId))
+        console.log(listOfSenderNfts)
+      } else {
+        setListOfSenderNfts([...listOfSenderNfts, nft])
       }
-      setListOfSenderNfts([...listOfSenderNfts, nft])
     } else {
       if (listOfReceiverNfts.some((nftInList) => nftInList.tokenId === nft.tokenId)) {
-        return
+        setListReceiverOfNfts(listOfReceiverNfts.filter((nftInList) => nftInList.tokenId !== nft.tokenId))
+      } else {
+        setListReceiverOfNfts([...listOfReceiverNfts, nft])
       }
-      setListReceiverOfNfts([...listOfReceiverNfts, nft])
     }
+  }
+
+  const handleSwap = async () => {
+    const transaction = swap(defaultWalletAddress, listOfSenderNfts, listOfReceiverNfts, receiverWalletAddress)
+      .then((res) => {
+        createOffer({
+          variables: {
+            makerWalletAddress: defaultWalletAddress,
+            takerWalletAddress: receiverWalletAddress,
+            makerData: res,
+            makerNfts: listOfSenderNfts,
+            takerNfts: listOfReceiverNfts,
+          },
+        })
+        alert(`🎉 🥳 Order filled`)
+      }
+      )
+      .catch((err) => {
+        console.log(err)
+      })
   }
 
   useEffect(() => {
@@ -345,20 +296,17 @@ export const TradingPanel = () => {
 
             <TradingPanelWrapper>
               <TrdingPanel>
-                <Text>Your NFTs</Text>
                 <MyNftList>
+                  <Text>Your NFTs</Text>
                   <NftList handleNftClicked={handleNftClicked} showShadow={false} nftList={listOfSenderNfts} interactive size='small' />
-                  <ApproveWrapper>
-                    <ApproveCheckbox type='checkbox' />
-                    <MediumText>Approve trade</MediumText>
-                  </ApproveWrapper>
                 </MyNftList>
-                <Text>{receiverUsername}'s NFTs</Text>
                 <MyNftList>
+                  <Text>{receiverUsername}'s NFTs</Text>
                   <NftList handleNftClicked={handleNftClicked} showShadow={false} nftList={listOfReceiverNfts} interactive size='small' />
                 </MyNftList>
                 <TradeInfoWrapper>
-                  <TradeButton onClick={() => swap(senderAddress)}>Trade</TradeButton>
+                  <TradeButton onClick={handleSwap}>Trade</TradeButton>
+                  {/* <TradeButton onClick={(handlePart2)}>Part2</TradeButton> */}
                 </TradeInfoWrapper>
               </TrdingPanel>
             </TradingPanelWrapper>
