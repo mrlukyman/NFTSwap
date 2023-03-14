df03d1253e701363f16cee3efec0f6715b28f78e
Lukas Haring <lukasharing123@gmail.com>
2023-03-14 01:18:50 +0100

    added trade section to the homepage

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,6 +2,7 @@ import { Header } from '../components/Header';
 import { Hero } from '../components/Hero';
 import { Container } from '../styles/GlobalStyles';
 import { Footer } from '../components/Footer';
+import { TradeSection } from '../components/TradeSection';
 
 export const Home = () => {
   return (
@@ -9,6 +10,7 @@ export const Home = () => {
       <Container>
         <Header />
         <Hero />
+        <TradeSection />
       </Container>
       <Footer />
     </>
