0c46baf03fefae380945bd92c7084848da39f385
Lukas Haring <lukasharing123@gmail.com>
2023-08-17 22:50:32 +0200

    added color

--- client/src/styles/Colors.ts
+++ client/src/styles/Colors.ts
@@ -1,6 +1,7 @@
 export const Colors = {
   background: "#140C1F",
   cardBackground: 'linear-gradient(147.75deg, rgba(255, 255, 255, 0.1) 0%, rgba(255, 255, 255, 0.05) 100%)',
+  cardBackgroundFocus: 'linear-gradient(90deg,rgba(255, 255, 255, 0.06)  0%, rgba(255, 255, 255, 0.3) 100%);',
   buttonBackground: 'rgba(248, 29, 251, 0.05)',
   primary: "#F81DFB",
   text: "#fff",
